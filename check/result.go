054ad7617e3d497ee36399a0b019232f7d0aab5b
ushi <ushi@honkgong.info>
2022-03-02 21:42:46 +0100

    case

--- check/result.go
+++ check/result.go
@@ -5,12 +5,12 @@ import "fmt"
 type Status uint8
 
 const (
-	STATUS_OK   Status = 0
-	STATUS_FAIL Status = 1
+	StatusOK   Status = 0
+	StatusFail Status = 1
 )
 
 func (s Status) String() string {
-	if s == STATUS_OK {
+	if s == StatusOK {
 		return "OK"
 	}
 	return "FAIL"
@@ -35,9 +35,9 @@ type Result struct {
 }
 
 func OK(format string, args ...interface{}) Result {
-	return Result{STATUS_OK, fmt.Sprintf(format, args...)}
+	return Result{StatusOK, fmt.Sprintf(format, args...)}
 }
 
 func Fail(format string, args ...interface{}) Result {
-	return Result{STATUS_FAIL, fmt.Sprintf(format, args...)}
+	return Result{StatusFail, fmt.Sprintf(format, args...)}
 }
